6a524f6e33aa18e175a311629d69dacfbd2ced4f
rimjhimkrn-web <rimjhimkrn@gmail.com>
2026-08-28 10:08:43 +0530

    Add admin panel and Supabase configuration

---
+++ config.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://ebltviniygljwwseplph.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_8xzZUeubYp_nIxVRWEQdiw_2H4RkHAa";
+
+  const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+      SUPABASE_PUBLISHABLE_KEY
+      );
